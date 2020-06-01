6250fb9b15bcff99a2a9f6f2b57e3c8919890e02
Xiaocao <wdjoys@hotmail.com>
2020-06-01 22:49:11 +0800

    4

--- js/rem.js
+++ js/rem.js
@@ -1,26 +1,35 @@
-;(function(win) {
-    var docEl = win.document.documentElement,
-        tid;
-
-    function refreshRem() {
-        var width = docEl.getBoundingClientRect().width;
-        var rem = width / 100;//以750px为原稿，除以100可得各元素的rem
-        docEl.style.fontSize = rem + "px";
-        var actualSize = parseFloat(window.getComputedStyle(docEl)["font-size"]);
-        if (actualSize !== rem) {
-            var remScaled = rem / ( actualSize / rem );
-            docEl.style.fontSize = remScaled + "px"
-        }
-   }
-
-    function dbcRefresh() {
-        clearTimeout(tid);
-        tid = setTimeout(refreshRem, 100)
-    }
-    win.addEventListener('orientationchange', function() { dbcRefresh() }, false);
-    win.addEventListener("resize", function() { dbcRefresh() }, false);
-    win.addEventListener("pageshow", function(e) {
-        if (e.persisted) { dbcRefresh() }
-    }, false);
-    refreshRem();
-})(window);
+// 转化rem单位
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 750) {
+                docEl.style.fontSize = '100px';
+            } else {
+                docEl.style.fontSize = clientWidth / 100 + 'px';
+            }
+            var html = document.getElementsByTagName('html')[0];
+            var settedFs = settingFs = parseInt(100 * (clientWidth / 750));
+            var whileCount = 0;
+            while (true) {
+                var realFs = parseInt(window.getComputedStyle(html).fontSize);
+                var delta = realFs - settedFs;
+                //不相等
+                if (Math.abs(delta) >= 1) {
+                    if (delta > 0)
+                        settingFs--;
+                    else
+                        settingFs++;
+                    html.setAttribute('style', 'font-size:' + settingFs + 'px!important');
+                } else
+                    break;
+                if (whileCount++ > 100)
+                    break
+            }
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
