5b919e2632c896f34deadc8a832992795cb19e35
Xiaocao <wdjoys@hotmail.com>
2020-06-01 10:45:27 +0800

    9

--- js/rem.js
+++ js/rem.js
@@ -1,59 +1,40 @@
-;(function (designWidth, maxWidth) {
-    var doc = document,
-        win = window;
-    var docEl = doc.documentElement;
-    var tid;
-    var rootItem, rootStyle;
-
-    function refreshRem() {
-        var width = docEl.getBoundingClientRect().width;
-        if (!maxWidth) {
-            maxWidth = 540;
+;(function (doc, win) {
+    // 我们的规则是 窗口100%宽度的rem = 10rem
+    // 我们在写css的时候，也要按照这个标准来写
+    
+    /*
+        css 公式，假如设计稿的宽度是750px，那么w变量的元素，实际的rem值是
+    这是一个简单换算公式，实际项目中可以使用px2rem-loader配置
+        function c (w) {
+        const width100 = 750
+        const remClient = 10; 
+        return Number((10*(w/750)).toFixed(3))
         }
-        ;
-        if (width > maxWidth) {
-            width = maxWidth;
+    */
+    var pxOneRem = 0;
+    var remClient = 100;
+    var docEl = doc.documentElement,
+      resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+      recalc = function () {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) return;
+        clientWidth = clientWidth > 900 ? 900 : clientWidth;
+        pxOneRem = clientWidth / remClient;
+        docEl.style.fontSize = pxOneRem + 'px';
+        // 纠错函数
+        function adapt(){  
+          var d = window.document.createElement('div');
+          d.style.width = '1rem';
+          d.style.display = "none";
+          var head = window.document.getElementsByTagName('head')[0];
+          head.appendChild(d);
+          var defaultFontSize = parseFloat(window.getComputedStyle(d, null).getPropertyValue('width'));
+          return defaultFontSize
         }
-        //与淘宝做法不同，直接采用简单的rem换算方法1rem=100px
-        var rem = width * 100 / designWidth;
-        //兼容UC开始
-        rootStyle = "html{font-size:" + rem + 'px !important}';
-        rootItem = document.getElementById('rootsize') || document.createElement("style");
-        if (!document.getElementById('rootsize')) {
-            document.getElementsByTagName("head")[0].appendChild(rootItem);
-            rootItem.id = 'rootsize';
-        }
-        if (rootItem.styleSheet) {
-            rootItem.styleSheet.disabled || (rootItem.styleSheet.cssText = rootStyle)
-        } else {
-            try {
-                rootItem.innerHTML = rootStyle
-            } catch (f) {
-                rootItem.innerText = rootStyle
-            }
-        }
-        //兼容UC结束
-        docEl.style.fontSize = rem + "px";
-    };
-    refreshRem();
-
-    win.addEventListener("resize", function () {
-        clearTimeout(tid); //防止执行两次
-        tid = setTimeout(refreshRem, 300);
-    }, false);
-
-    win.addEventListener("pageshow", function (e) {
-        if (e.persisted) { // 浏览器后退的时候重新计算
-            clearTimeout(tid);
-            tid = setTimeout(refreshRem, 300);
-        }
-    }, false);
-
-    if (doc.readyState === "complete") {
-        doc.body.style.fontSize = "16px";
-    } else {
-        doc.addEventListener("DOMContentLoaded", function (e) {
-            doc.body.style.fontSize = "16px";
-        }, false);
-    }
-})(100, 750);
+        pxOneRem = pxOneRem * pxOneRem / adapt();
+        docEl.style.fontSize = pxOneRem + 'px'
+      };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+  })(document, window);
