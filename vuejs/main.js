27f5292ce7bb00569c59afd3165cc0d6f95c20ba
Xiaocao <wdjoys@hotmail.com>
2020-06-01 16:08:56 +0800

    12

--- vuejs/main.js
+++ vuejs/main.js
@@ -14,13 +14,13 @@ Vue.component('my-file', {
 
 Vue.component('my-company', {
   template: `
-      <div class='compang-card'>
+    <div class='compang-card'>
       <div id="name">
           <h4>宁夏天地奔牛实业集团有限公司</h4>
           <a href="http://www.khb.com/index.php?g=Wap&m=Index&a=content&token=uzeuwh1444287382&classid=5078&id=2951">点击了解详情</a>
       </div>
       <div id="logo"><img src="./img/logo.png" alt=""></div>
-    <div>
+    </div>
     `
 })
 
